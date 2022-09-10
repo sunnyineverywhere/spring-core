cfde4ec61c2d912b3d4140c9bdf1b62535868502
sunnyineverywhere <sunnyinsummer@ewhain.net>
2022-09-10 13:28:02 +0900

    새로운 구조와 할인 정책 적용
    - client 코드를 변경하지 않고도 AppConfig 내의 객체 생성 코드만 변경하면 새로운 정책 도입 가능
    - 사용 영역 X, 구성 영역 O

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDisountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -19,7 +22,16 @@ public class AppConfig {
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDisountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
+
     }
 }
