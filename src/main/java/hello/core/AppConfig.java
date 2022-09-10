fd5a539188236b104b73f7a45e01215f1d25edaf
sunnyineverywhere <sunnyinsummer@ewhain.net>
2022-09-10 15:43:43 +0900

    스프링 프레임워크로 개선

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,27 +9,36 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.ManagedBean;
 
 /**
  * 어플리케이션의 실제 동작에 필요한 구현 객체를 생성
  * 생성한 객체 인스턴스의 객체를 연결을 통해서 주입
  * 생성자를 통해서 주입(연결한다) = Injection * * *
  */
+@Configuration // 외부 주입 설정 정보
 public class AppConfig {
 
     // 생성자 주입
+    @Bean // 스프링 컨테이너에 등록
     public MemberService memberService(){
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
 
